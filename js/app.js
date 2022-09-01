fe639f4bd0b22e8212115857153e705575fee4e8
Julian Barker <julian.c.barker@gmail.com>
2022-09-01 14:23:15 -0700

    complete JS

--- js/app.js
+++ js/app.js
@@ -8,15 +8,25 @@ const Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  this.items.push(new CartItem(product, quantity));
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage['cart'] = JSON.stringify(this.items);
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
-  // Note: You will have to decide what kind of parameter to pass in here!
+  // Note: You will have to decide what kind of parameter to pass in here! ==> 
+  for (let i in this.items) {
+    if (this.items[i].product.name === item.product.name) {
+      console.log(`i = ${i}`);
+      console.log(this.items);
+      this.items.splice(i, 1);
+      console.log(this.items);
+    }
+  }
 };
 
 const CartItem = function(product, quantity) {
